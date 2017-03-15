032f50dafe0660b3429f40cfb39fe17de5c5741f
rongxanh88 <rongxanh88@gmail.com>
2017-03-15 09:23:09 -0600

    first commit of sum exercise

---
+++ summation_exercise.rb
@@ -0,0 +1,16 @@
+def square_then_sum(min_value, max_value)
+  sum = (min_value..max_value).to_a.map {|number| number * number}
+  return sum.inject {|total, num| total + num}
+end
+
+def sum_then_square(min_value, max_value)
+  sum = (min_value..max_value).to_a.inject {|total, num| total + num}
+  sum = sum * sum
+  return sum
+end
+
+value1 = square_then_sum(1,10)
+value2 = sum_then_square(1,10)
+total = value2 - value1
+
+puts "#{value2} - #{value1} = #{total}."
